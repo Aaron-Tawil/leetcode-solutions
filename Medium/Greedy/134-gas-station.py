9a8289808e86ab327dd8ddadca2edfe989f94f5d
Aaron Tawil <aarondavidt@mail.tau.ac.il>
2025-07-20 20:46:01 +0300

    134

--- Medium/Greedy/134-gas-station.py
+++ Medium/Greedy/134-gas-station.py
@@ -1,1 +1,18 @@
-# Solution for LeetCode problem 134: Gas Station
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        if sum(cost)>sum(gas):
+            return -1
+        
+        # the insight is that if we pass the condition it is guarnteed there is a solution
+        start = tank = 0
+        n = len(gas)
+        for i in range(n):
+            tank += gas[i] - cost[i]
+            if tank<0:
+                start = i+1
+                tank = 0
+
+        return start 
+
+        
