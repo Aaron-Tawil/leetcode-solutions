8462d69bdb1ae905e16efc44100c7c963aa1d1cb
Aaron Tawil <aarondavidt@mail.tau.ac.il>
2025-10-05 19:11:25 +0300

    347

---
+++ Medium/Heap_(Priority_Queue)/347-top-k-frequent-elements.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        return [t[0] for t in counter.most_common(k)]
